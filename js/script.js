ffc7b5cfe44b9d5c6299328f09b8e1842eb132ca
maryryan <marym.ryan@hotmail.com>
2016-05-03 19:38:57 -0500

    more tooltip

--- js/script.js
+++ js/script.js
@@ -120,48 +120,66 @@ d3.csv("data/degree_cost.csv", function(error, data) {
   console.log(PRICE[0][0][0]["values"]);
     //PRICE[array][array][SPECIFY FOR DEGREE]["values"][SPECIFY FOR YEAR]["price"]
 
-  degree.selectAll(".dot")
+degree.selectAll(".dot")
     .data(function(d) {
+          //The `city` selection already holds the data for our three lines.
+          //So we'll use it to draw our dots. The value for each array of dots
+          //will be the array of values attached to each line:
       return d.values;
     })
-    .enter().append("circle")
+    .enter().append("circle") //Add a new circle for each data point in the array.
     .attr("class", "dot")
     .attr("cx", function(d) {
-      return x(d.date);
+        return x(d.date); //Position accordingly.
     })
     .attr("cy", function(d) {
-      return y(d.price);
+        return y(d.price); //Position accordingly.
     })
     .attr("r", 5)
     .on("mouseover", function(d) {
 
-      var displayDate = moment(d.date).format("YYYY");
-      var displayVal = "$"+d.price;
+            //We're using the Moment.js library to get a month and year for our tooltip.
+            //We're using Moment.js because our dates are in the js date format.
+        var displayDate = moment(d.date).format("YYYY");
+        var displayVal = "$"+d.price+"0";
 
-      $(".d3-tip").html(
-        "<div class='name'>"+d.name+"</div>"+
-        "<div class='date'>"+displayDate+": </div>"+
-        "<div class='price'>"+displayVal+"</div>"
+            //Append the values to the tooltip with some markup.
+        $(".tt").html(
+          "<div class='name'>"+d.name+"</div>"+
+          "<div class='date'>"+displayDate+": </div>"+
+          "<div class='price'>"+displayVal+"</div>"
         )
 
-      $(".d3-tip").show();
+            //Show the tooltip.
+        $(".tt").show();
 
-      d3.select(this).style("opacity", 1);
+            //Make the dot visible.
+        d3.select(this).style("opacity", 1);
+            
     })
     .on("mousemove", function(d) {
 
-      var xPos = d3.mouse(this)[0] + margin.left + 10;
-      var yPos = d3.mouse(this)[1] + margin.top + 10;
+            //Get the mouse position relative to the .chart div.
+            //Add the margin.left and margin.top values to make the div set properly in the .chart.
+            //Add 10px to each so the tooltip is offset appropriately.
+        var xPos = d3.mouse(this)[0] + margin.left + 10;
+        var yPos = d3.mouse(this)[1] + margin.top + 10;
 
-      $(".d3-tip").css({
-        "left": xPos + "px",
-        "top": yPos + "px"
-      })
+            //Use jQuery to position the .tt div with the .chart div.
+            //See the CSS for important style info here. 
+        $(".tt").css({
+            "left": xPos + "px",
+            "top": yPos + "px"
+        })
+
     })
     .on("mouseout", function(d) {
-      d3.select(this).style("opacity", 0);
-      $(".d3-tip").hide();
+            //Turn this dot's opacity back to 0
+            //And hide the tooltip.
+        d3.select(this).style("opacity", 0);
+        $(".tt").hide();
     })
+
 
   // var tip = d3.tip()
   // .attr('class', 'd3-tip')
@@ -190,7 +208,7 @@ d3.csv("data/degree_cost.csv", function(error, data) {
   degree.append("path")
     .attr("class", "line")
     .attr("d", function(d) { return line(d.values); })
-    .attr("stroke", "gray")
+    .attr("stroke", "gray");
     // .on('mouseover', tip.show)
     // //fixing the tooltip so it appears close to the line
     // .on("mousemove", function () {
